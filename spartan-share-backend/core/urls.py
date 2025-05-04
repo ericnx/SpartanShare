aac4fdbeb248b68f1c74a910c76042fbf648790f
Eric Nguyen <eriicnguyen1@gmail.com>
2025-05-03 19:07:27 -0700

    added applied projects url

--- spartan-share-backend/core/urls.py
+++ spartan-share-backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProjectViewSet, ApplicationViewSet, MyProjectsView, list_saved_projects, apply_to_project
+from .views import UserViewSet, ProjectViewSet, ApplicationViewSet, MyProjectsView, AppliedProjectsView, list_saved_projects, apply_to_project
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,5 +12,6 @@ urlpatterns = [
     path('my-projects/', MyProjectsView.as_view(), name='my-projects'),
     # path('save-project/<int:project_id>/', toggle_save_project),
     path('saved-projects/', list_saved_projects),
+    path('applied-projects', AppliedProjectsView.as_view(), name='applied-projects' ),
     path('projects/<int:project_id>/apply/', apply_to_project, name='apply-to-project'),
 ]
